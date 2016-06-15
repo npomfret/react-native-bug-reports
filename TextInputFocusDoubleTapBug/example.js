c9c78939433adf866503c6222bede5adc3a7bee2
npomfret <nick-github@snowmonkey.co.uk>
2016-06-15 18:50:47 +0100

    demo

--- TextInputFocusDoubleTapBug/example.js
+++ TextInputFocusDoubleTapBug/example.js
@@ -6,7 +6,8 @@ export default class Example extends React.Component {
     super(props);
     this.state = {
       example1Count: 0,
-      example2Count: 0
+      example2Count: 0,
+      example3Count: 0
     }
   }
 
@@ -18,14 +19,18 @@ export default class Example extends React.Component {
     this.setState({example2Count: this.state.example2Count + 1});
   }
 
+  example3() {
+    this.setState({example3Count: this.state.example3Count + 1});
+  }
+
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 40, backgroundColor: 'white'}}>
 
         <Text>Example 1:</Text>
-        <Text>This works as expected:</Text>
+        <Text>Requires 1 tap as expected:</Text>
         <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: 20}}>
-          <TextInput style={{height: 40, width: 100, borderWidth: 1, marginRight: 8, padding: 4}} placeholder="focus me"/>
+          <TextInput ref="example1" style={{height: 40, width: 100, borderWidth: 1, marginRight: 8, padding: 4}} placeholder="focus me"/>
 
           <TouchableHighlight onPress={this.example1.bind(this)} style={{borderWidth: 1, backgroundColor: 'black', marginRight: 8}}>
             <Text style={{color: 'white'}}>Press me</Text>
@@ -35,16 +40,32 @@ export default class Example extends React.Component {
         </View>
 
         <Text>Example 2 (inside a scroll view):</Text>
-        <Text>This works as above for android, but IOS requires 2 taps:</Text>
+        <Text>Requires 2 taps:</Text>
         <ScrollView>
           <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-            <TextInput style={{height: 40, width: 100, borderWidth: 1, marginRight: 8, padding: 4}} placeholder="focus me"/>
+            <TextInput ref="example2" style={{height: 40, width: 100, borderWidth: 1, marginRight: 8, padding: 4}} placeholder="focus me"/>
 
             <TouchableHighlight onPress={this.example2.bind(this)} style={{borderWidth: 1, backgroundColor: 'black', marginRight: 8}}>
               <Text style={{color: 'white'}}>Press me</Text>
             </TouchableHighlight>
 
             <Text>Count: {this.state.example2Count}</Text>
+
+          </View>
+        </ScrollView>
+
+        <Text>Example 3 (with keyboardShouldPersistTaps=true):</Text>
+        <Text>Requires 1 tap as expected:</Text>
+        <ScrollView keyboardShouldPersistTaps={true}>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+            <TextInput ref="example3" style={{height: 40, width: 100, borderWidth: 1, marginRight: 8, padding: 4}} placeholder="focus me"/>
+
+            <TouchableHighlight onPress={this.example3.bind(this)} style={{borderWidth: 1, backgroundColor: 'black', marginRight: 8}}>
+              <Text style={{color: 'white'}}>Press me</Text>
+            </TouchableHighlight>
+
+            <Text>Count: {this.state.example3Count}</Text>
+
           </View>
         </ScrollView>
 
